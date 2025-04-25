ec10e5cfe4c2074e41242474afbc8e2526da66c9
sakshiskulal <sakshi.23cs131@sode-edu.in>
2025-04-25 09:49:19 +0530

    Add files via upload

---
+++ Pgrm4.java
@@ -0,0 +1,15 @@
+public class Pgrm4 {
+
+  public static void main(String[] args){
+
+    System.out.println(4*3+7*2);
+
+    System.out.println(15/3*4);
+
+    System.out.println(10/2*6/3);
+
+    System.out.println(10+2*5+3);
+
+  }
+
+}
